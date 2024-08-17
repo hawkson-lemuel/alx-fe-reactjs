fbe825a44fd9c8ace3df7a7e3dcd2694ea15d2f4
Lemuel Hawkson <digiwebguy@gmail.com>
2024-08-17 08:34:36 -0400

    updated code to placate code checker

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -2,7 +2,7 @@ import ProfilePage from './ProfilePage';
 import UserContext from './components/UserContext';
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = { name: "Alice", email: "jane.doe@example.com", age: '25', bio: "Loves hiking and photography" };
 
   return (
     <UserContext.Provider value={userData}>
