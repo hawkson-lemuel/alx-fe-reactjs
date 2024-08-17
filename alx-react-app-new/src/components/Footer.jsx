4afc4cbed910fc0661c3abe6eb39556f78754bd8
Lemuel Hawkson <digiwebguy@gmail.com>
2024-08-17 08:03:25 -0400

    - Duplicated alx-react-app - Added inline styling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+    return (
+        <footer style={{backgroundColor:'orange'}}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{backgroundColor:'green'}}>
+            <p style={{fontSize:'28px'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
