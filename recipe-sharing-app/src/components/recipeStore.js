6e6e762fc8141b59ba2c1627a12d09a5705d6784
Lemuel Hawkson <digiwebguy@gmail.com>
2024-08-25 09:14:20 -0400

    fixed code checker issue

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,17 +2,17 @@ import create from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
-  searchTerm: '',
   filteredRecipes: [],
 
   addRecipe: (newRecipe) =>
+  
     set((state) => ({
       recipes: [...state.recipes, newRecipe],
       filteredRecipes: [...state.recipes, newRecipe].filter((recipe) =>
         recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
       ),
     })),
-
+    setRecipes: (recipes) => set({ recipes }),
   setSearchTerm: (term) => 
     set((state) => {
       const filtered = state.recipes.filter((recipe) =>
