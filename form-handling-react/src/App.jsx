2c064d7de7f2d60ff678cef8fbbb144bfb61ba01
Lemuel Hawkson <digiwebguy@gmail.com>
2024-08-31 14:40:19 -0400

    initial commit. Created user registration form and added form validation with formik

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import RegistrationForm from './components/RegistrationForm';
+import FormikForm from './components/FormikForm';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>User Registration</h1>
+      <FormikForm />
+    </div>
+  );
+};
+
+export default App;
