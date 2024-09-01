fb0938de8edbec667f4133a94bee7d88cc12a33a
Lemuel Hawkson <digiwebguy@gmail.com>
2024-09-01 01:41:46 -0400

    fixed bad naming for PostsComponent

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import PostsComponent from './components/ PostsComponent';
+import PostsComponent from './components/PostsComponent';
 
 const queryClient = new QueryClient();
 
