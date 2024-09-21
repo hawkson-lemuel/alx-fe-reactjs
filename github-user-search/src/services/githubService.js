0d48444ff289c58a3754153163c5ea237e3ac615
Lemuel Hawkson <digiwebguy@gmail.com>
2024-09-21 21:51:15 +0000

    updates to pass code checker.

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,19 +1,17 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.github.com/search/users';
-
-export const fetchUserData = async (username, location, repos, page = 1, perPage = 30) => {
+export const fetchUserData = async (username, location, minRepos, page = 1, perPage = 30) => {
     console.log("Triggered");
 
     const queryParts = [];
     if (username) queryParts.push(username);
     if (location) queryParts.push(`location:${location}`);
-    if (repos) queryParts.push(`repos:>${repos}`);
+    if (minRepos) queryParts.push(`repos:>${minRepos}`);
 
     const query = queryParts.join(' ');
 
     try {
-        const response = await axios.get(`${BASE_URL}?q=${query}&page=${page}&per_page=${perPage}`);
+        const response = await axios.get(`https://api.github.com/search/users?q=${query}&page=${page}&per_page=${perPage}`);
         return {
             totalCount: response.data.total_count,
             items: response.data.items,
